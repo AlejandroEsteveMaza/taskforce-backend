b05745a1d3d88f66a139cbb2835c7fae87121434
Alejandro Esteve Maza <alex.esteve98@gmail.com>
2024-02-25 13:32:06 +0100

    Project Creation

---
+++ src/main/java/com/alejandroem/taskforce/backend/TaskForceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alejandroem.taskforce.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskForceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskForceBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alejandroem/taskforce/backend/TaskForceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alejandroem.taskforce.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskForceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
